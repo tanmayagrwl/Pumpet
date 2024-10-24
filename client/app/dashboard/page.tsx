d39b9a3807ba3473cd3685815f020aacbb550725
tanmayagrwl <tanmayhp78@gmail.com>
2024-10-24 16:43:40 +0530

    add site header

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import SiteHeader from '@/components/site-header'
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        <SiteHeader/>
+    </div>
+  )
+}
+
+export default Page
